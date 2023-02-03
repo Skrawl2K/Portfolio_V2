36cbc576c5ccdeec2d13c512ff7f657f3c7b3534
Skrawl2K <kev-koslowski@gmx.de>
2023-02-03 14:54:38 +0100

    corrected deploy errors

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -12,7 +12,7 @@ const Links = () => {
                 <a href='https://github.com/Skrawl2K'><img className='socialIcon1' src='./img/github-mark-white.svg' alt="github"></img>Giuthub</a>
 
                 <a href="https://www.linkedin.com/in/kevin-koslowski-39081a262/"><img className='socialIcon2' src='./img/linkedin-svgrepo-com.svg' alt="linkedin"></img>LinkedIn</a>
-                <img></img>
+                <img alt="instaffo"></img>
             </ul>
 
         </>
