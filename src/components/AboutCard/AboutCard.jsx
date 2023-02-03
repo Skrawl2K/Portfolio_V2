79414a96ef27760e5a5bb5703bd2bf2e06925c16
Skrawl2K <kev-koslowski@gmx.de>
2023-02-03 14:53:08 +0100

    corrected deploy errors

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -3,18 +3,9 @@ import './AboutCard.css'
 const AboutCard = () => {
     return (
         <div className="row">
-            <img className='imgSize' src='./img/Kev_laptop.png'></img>
+            <img className='imgSize' src='./img/Kev_laptop.png' alt='kev'></img>
             <p className='text'>Hi & welcome to my Portfolio. Here you can find some screenshots & links to my projects & some additional information about me.
             </p>
-            {/* <ul className='iconWrapper'>
-                <a href='https://github.com/Skrawl2K' >
-                    <img className='socialIcons' src='./img/github-mark-white.svg'></img>
-                </a>
-                <a href=''>
-                    <img className='socialIcons' src='./img/linkedin-svgrepo-com.svg'></img>
-                </a>
-                <img></img>
-            </ul> */}
         </div >
     )
 }
--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -9,9 +9,9 @@ const Links = () => {
             <h1 className="headlines ">Contact Links</h1>
 
             <ul className='iconWrapper'>
-                <a href='https://github.com/Skrawl2K'><img className='socialIcon1' src='./img/github-mark-white.svg'></img>Giuthub</a>
+                <a href='https://github.com/Skrawl2K'><img className='socialIcon1' src='./img/github-mark-white.svg' alt="github"></img>Giuthub</a>
 
-                <a href="https://www.linkedin.com/in/kevin-koslowski-39081a262/"><img className='socialIcon2' src='./img/linkedin-svgrepo-com.svg'></img>LinkedIn</a>
+                <a href="https://www.linkedin.com/in/kevin-koslowski-39081a262/"><img className='socialIcon2' src='./img/linkedin-svgrepo-com.svg' alt="linkedin"></img>LinkedIn</a>
                 <img></img>
             </ul>
 
