2821e19f76aa0adb17713e370a96e71173108f49
Skrawl2K <107536782+Skrawl2K@users.noreply.github.com>
2023-02-06 14:08:53 +0100

    further modificatins to: Navbar , Links page & Landing Page

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -6,15 +6,14 @@ const Links = () => {
     return (
         <>
             <Navbar />
-            <h1 className="headlines ">Contact Links</h1>
-
-            <ul className='iconWrapper'>
-                <a href='https://github.com/Skrawl2K'><img className='socialIcon1' src='./img/github-mark-white.svg' alt="github"></img>Giuthub</a>
-
-                <a href="https://www.linkedin.com/in/kevin-koslowski-39081a262/"><img className='socialIcon2' src='./img/linkedin-svgrepo-com.svg' alt="linkedin"></img>LinkedIn</a>
-                <img alt="instaffo"></img>
-            </ul>
-
+            <div className="main">
+                <h1 className="headlines ">Contact Links</h1>
+                <ul className='iconWrapper'>
+                    <a className="LinkWrapper" href='https://github.com/Skrawl2K'><img className='socialIcon1' src='./img/github-mark-white.svg' alt="github"></img>Github</a>
+                    <a className="LinkWrapper" href="https://www.linkedin.com/in/kevin-koslowski-39081a262/"><img className='socialIcon2' src='./img/linkedin-svgrepo-com.svg' alt="linkedin"></img>LinkedIn</a>
+                    <h2>Or you can find me on "Instaffo.com"</h2>
+                </ul>
+            </div>
         </>
     )
 };
